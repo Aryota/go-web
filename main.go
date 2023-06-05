f17e7bc5f1e72bd58549473cb41e8c075ab30c0f
ryota akabane <51541610+Aryota@users.noreply.github.com>
2023-06-05 18:31:40 -0400

    DeleitPage Transition

--- main.go
+++ main.go
@@ -30,8 +30,40 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("view/main.html"))
 
-		data := PageData{
-			Title: "Checkout!",
+		if r.Method == "POST" {
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+
+			numberStr := r.Form.Get("number")
+			number, err := strconv.Atoi(numberStr)
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				return
+			}
+
+			first := generateRandomNumber()
+			second := generateRandomNumber()
+			third := generateRandomNumber()
+
+			newNumber := NumberData{
+				Number: number,
+				First:  first,
+				Second: second,
+				Third:  third,
+			}
+
+			tableData.Numbers = append(tableData.Numbers, newNumber)
+		}
+
+		data := struct {
+			PageData
+			TableData
+		}{
+			PageData:  PageData{Title: "Checkout!"},
+			TableData: tableData,
 		}
 
 		err := tmpl.ExecuteTemplate(w, "main.html", data)
@@ -40,18 +72,19 @@ func main() {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
-
 	})
 
 	http.HandleFunc("/javascript/number.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "view/javascript/number.js")
 	})
 
+	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		tableData = TableData{}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
 	fs := http.FileServer(http.Dir("view/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/process", processNumber)
-	http.HandleFunc("/reset", resetTable)
 
 	http.ListenAndServe(":8080", nil)
 }
@@ -59,51 +92,4 @@ func main() {
 func generateRandomNumber() int {
 	rand.Seed(time.Now().UnixNano())
 	return 1 + rand.Intn(60)
-}
-
-func processNumber(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	numberStr := r.Form.Get("number")
-	number, err := strconv.Atoi(numberStr)
-	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	first := generateRandomNumber()
-	second := generateRandomNumber()
-	third := generateRandomNumber()
-
-	newNumber := NumberData{
-		Number: number,
-		First:  first,
-		Second: second,
-		Third:  third,
-	}
-
-	tableData.Numbers = append(tableData.Numbers, newNumber)
-
-	tmpl := template.Must(template.ParseFiles("view/table.html"))
-
-	err = tmpl.ExecuteTemplate(w, "table", tableData)
-	if err != nil {
-		log.Println("Failed to render template:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-}
-
-func resetTable(w http.ResponseWriter, r *http.Request) {
-	tableData.Numbers = nil
-	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
